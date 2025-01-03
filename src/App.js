f5a61b61038c358b43e339e725235ab1b2103b67
Felix Pang <felixpang625@gmail.com>
2025-01-04 02:10:18 +0800

    components

--- src/App.js
+++ src/App.js
@@ -1,9 +1,17 @@
+import React, { useState } from 'react'
+import SearchBar from "./components/SearchBar/SearchBar"
+import SearchResults from "./components/SearchResults/SearchResults"
+import Playlist from "./components/Playlist/Playlist"
 import './App.css';
 
+
 function App() {
+  const [searchResults, setSearchResults] = useState([]);
+  const [playlistName, setPlaylistName] = useState("New Playlist");
+  const [playlistTracks, setPlaylistTracks] = useState([])
   return (
     <div className="App">
-      <h1>My React App</h1>
+      <h1>Jammming</h1>
     </div>
   );
 }
